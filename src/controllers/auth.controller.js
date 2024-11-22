d67f852a82fdb10ac9c0a035bb5705ebed46958f
mariana.urzainqui <marianaurzainqui@gmail.com>
2024-11-22 10:50:35 -0300

    changing verification url

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -79,7 +79,7 @@ export const registerUserController = async (req, res) => {
             expiresIn: '1d'
         })
 
-        const url_verification = `https://backend-proyecto-repaso.vercel.app/api/auth/verify/${verificationToken}`
+        const url_verification = `${URL_FRONT}/api/auth/verify/${verificationToken}`
 
         await sendEmail({
             to: email,
